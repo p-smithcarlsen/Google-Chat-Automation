124c5dd112e51c2ff40bd40993616be211b2a60b
philip.smith.carlsen <philip.smith.carlsen@dk.pwc.com>
2020-08-21 10:22:36 +0200

    minor changes

--- Main.py
+++ Main.py
@@ -2,7 +2,9 @@ import webhook_quickstart
 
 
 def main():
-    webhook_quickstart.quickstart("Hej Lars! Velkommen til vores chat room :)")
+    chat_message = "Hej alle! Velkommen til gruppen. Dette chat rum bruges til at teste Google Chat API'et. " \
+                   "Dette er en chat besked genereret af et Python script. :)"
+    webhook_quickstart.quickstart(chat_message)
 
 
 if __name__ == '__main__':
