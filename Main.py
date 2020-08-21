e73dc81099dd9214b06746cc7289dcba6f072ed6
philip.smith.carlsen <philip.smith.carlsen@dk.pwc.com>
2020-08-21 10:03:32 +0200

    Implemented initial codebase

---
+++ Main.py
@@ -0,0 +1,9 @@
+import webhook_quickstart
+
+
+def main():
+    webhook_quickstart.quickstart("Hej Lars! Velkommen til vores chat room :)")
+
+
+if __name__ == '__main__':
+    main()
---
+++ webhook_quickstart.py
@@ -0,0 +1,24 @@
+from json import dumps
+
+from httplib2 import Http
+
+
+def quickstart(message_to_chat: str):
+    """Hangouts Chat incoming webhook quickstart."""
+    url = 'https://chat.googleapis.com/v1/spaces/AAAAwUkULtw/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=4Qr5a0_wtQXJWsLEpEyF1KpNTSWxQELOmLgAnwF21Vo%3D'
+    bot_message = {
+        'text' : message_to_chat
+    }
+
+    message_headers = {'Content-Type': 'application/json; charset=UTF-8'}
+
+    http_obj = Http()
+
+    response = http_obj.request(
+        uri=url,
+        method='POST',
+        headers=message_headers,
+        body=dumps(bot_message),
+    )
+
+    print(response)
