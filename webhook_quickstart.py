a3909cc6a13f8b93cb88b7be52bbd59260c8cc90
philip.smith.carlsen <philip.smith.carlsen@dk.pwc.com>
2020-08-21 13:12:30 +0200

    minor changes

--- webhook_quickstart.py
+++ webhook_quickstart.py
@@ -5,8 +5,13 @@ from httplib2 import Http
 
 def quickstart(message_to_chat: str):
     """Hangouts Chat incoming webhook quickstart."""
-    url = 'https://chat.googleapis.com/v1/spaces/AAAAwUkULtw/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=4Qr5a0_wtQXJWsLEpEyF1KpNTSWxQELOmLgAnwF21Vo%3D'
-    # url for Application Errors chat room: 'https://chat.googleapis.com/v1/spaces/AAAABwSxnOU/messages?key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&token=euU7Cd6lDvff5VWPU1u3pEmRUPeh3Y-UyWmo89pHU0M%3D'
+    url = 'https://chat.googleapis.com/v1/spaces/AAAAwUkULtw/messages?' \
+          'key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&' \
+          'token=4Qr5a0_wtQXJWsLEpEyF1KpNTSWxQELOmLgAnwF21Vo%3D'
+    # url for Application Errors chat room:
+    # 'https://chat.googleapis.com/v1/spaces/AAAABwSxnOU/messages?
+    # key=AIzaSyDdI0hCZtE6vySjMm-WEfRq3CPzqKqqsHI&
+    # token=euU7Cd6lDvff5VWPU1u3pEmRUPeh3Y-UyWmo89pHU0M%3D'
     bot_message = {
         'text' : message_to_chat
     }
